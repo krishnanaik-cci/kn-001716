7e21e11fc77795ea343135a57e3cbc45f0535739
CREATIVECAPSULE\krishna.naik <krishna.naik@creativecapsule.com>
2022-10-28 19:00:18 +0530

    LYB-001716-300x600 *Working on the initial development - In Progress

--- LYB-001716/300x600/300x600/javascript/main.js
+++ LYB-001716/300x600/300x600/javascript/main.js
@@ -28,7 +28,7 @@ function init() {
 	getElem("isiText").innerHTML = isiText;
 	timeline = new TimelineMax({ onComplete: finishTimeline });
 	timeline
-		.set([getElem("cover"), getElem("border"), getElem("background"), getElem("hotspot"), getElem("disclaimer"), getElem("text-2"), getElem("isi"), getElem("isiHeader"),
+		.set([getElem("cover"), getElem("border"), getElem("background"), getElem("hotspot"), getElem("isi-indicator-text"), getElem("disclaimer"), getElem("lybalvi-logo"), getElem("text-2"), getElem("isi"), getElem("isiHeader"),
 		getElem("isiHeaderText"), getElem("isiTextHolder"), getElem("isiText")], { autoAlpha: 1 });
 
 	startAnimation();
@@ -41,7 +41,26 @@ var lastFrameTimeScale = 1.25;
 function startAnimation() {
 	var speed = 1 * timeScale;
 	startISIScroll(isiDelay, isiScrollTime, "pause~" + isiFinalPause);
+	timeline
+	.addLabel("frame1")
+	.to(getElem("lybalvi-logo"), speed, {x:-300, delay: 2}, "frame1")
+	.to(getElem("victoria-img"), speed, {autoAlpha:0, x:300},"frame1")
+	.to(getElem("text-one"), speed, {autoAlpha:0, x:300},"frame1")
 
+	.addLabel("frame2")
+	.to(getElem("copper-bar"), speed, {autoAlpha:1, x:0, delay:.5}, "frame2")
+	.to(getElem("text-two"), speed, {autoAlpha:1, x:0, delay:.5}, "frame2")
+	.to(getElem("victoria-img"), speed, {autoAlpha:1, x:0, delay:.5},"-=1.2", "frame2")
+	.to(getElem("text-one"), speed, {autoAlpha:1, x:0, delay:.5},"-=1.5", "frame2")
+	.to(getElem("lybalvi-logo"), speed, {autoAlpha:1,x:0, y:- 56, delay: 2}, "frame3")
+
+	.addLabel("frame3")
+	.to(getElem("victoria-img"), speed, {autoAlpha:0, x:-300, delay:.5}, "frame3")
+	.to(getElem("text-one"), speed, {autoAlpha:1, x:-300, delay:.5}, "frame3")
+	.to(getElem("text-two"), speed, {autoAlpha:0, x:0, delay:.5}, "frame3")
+	.to(getElem("copper-bar"), speed, {autoAlpha:0, x:0, delay:.5}, "frame3")
+	.to(getElem("lybalvi-logo"), speed, {autoAlpha:1, delay: 1}, "frame3")
+	.to(getElem("cta"), speed, {autoAlpha:1, x:0, delay:2}, "frame3")
 }
 
 
